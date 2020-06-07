921125941760c99124c3973ec422ec2b2a0937eb
赵博玉 <1904719046@qq.com>
2020-06-07 08:35:41 +0800

    拍照存储

--- src/main/java/com/zsc/museum/controller/StaffController.java
+++ src/main/java/com/zsc/museum/controller/StaffController.java
@@ -32,8 +32,6 @@ public class StaffController {
 
 
 
-
-
     @Autowired
     public StaffService staffService;
 
@@ -61,17 +59,26 @@ public class StaffController {
         }
     }
 
-    //拍照并把照片名称存入数据库
-    @RequestMapping(value = "/photo", method = {RequestMethod.POST, RequestMethod.GET})
-    public String imgFileName(HttpServletRequest request) {
-        String imgId = request.getParameter("imgId");
-        String imgFileName="cultural"+imgId+".png";
+//    //拍照并把照片名称存入数据库
+//    @RequestMapping(value = "/photo", method = {RequestMethod.POST, RequestMethod.GET})
+//    public String imgFileName(HttpServletRequest request) {
+//        String imgId = request.getParameter("imgId");
+//        String imgFileName="cultural"+imgId+".png";
+//
+//        culturalService.imgFileName(imgFileName,imgId);
+//        return "pages/photo";
+//
+//    }
+//拍照并把照片名称存入数据库
+@RequestMapping(value = "/photosave", method = {RequestMethod.POST, RequestMethod.GET})
+public String imgFileName(HttpServletRequest request) {
+    String imgId = request.getParameter("imgId");
+    String imgFileName="cultural"+imgId+".png";
 
-        culturalService.imgFileName(imgFileName,imgId);
-        return "pages/photo";
+    culturalService.imgFileName(imgFileName,imgId);
+    return "pages/photoEdit";
 
-    }
-
+}
     //拍照并把照片名称存入数据库
 //    @RequestMapping("/photo")
 //    public @ResponseBody String imgFileName(HttpServletRequest request,String dataURL,String test) {
