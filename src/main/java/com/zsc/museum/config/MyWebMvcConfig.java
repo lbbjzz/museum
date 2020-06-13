503ef2340c5958a6c8d6c55c75e4642653d9c3f6
CJH <2422453089@qq.com>
2020-06-13 16:05:30 +0800

    版本1.0完工

--- src/main/java/com/zsc/museum/config/MyWebMvcConfig.java
+++ src/main/java/com/zsc/museum/config/MyWebMvcConfig.java
@@ -12,12 +12,37 @@ public class MyWebMvcConfig extends WebMvcConfigurerAdapter {
     //将jar文件下的对应静态资源文件路径对应到磁盘的路径(根据个人的情况修改"file:static/"的static的值
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        ApplicationHome h = new ApplicationHome(getClass());
+//        File jarF = h.getSource();
+//        String dirPath = jarF.getParentFile().toString()+"/upload/";
+//
+//        registry.
+//                addResourceHandler("/upload/**"). addResourceLocations("file:/"+dirPath);
+
         ApplicationHome h = new ApplicationHome(getClass());
         File jarF = h.getSource();
         String dirPath = jarF.getParentFile().toString()+"/upload/";
 
-        registry.
-                addResourceHandler("/upload/**"). addResourceLocations("file:/"+dirPath);
+
+        String os = System.getProperty("os.name");
+
+        System.out.println(os);
+
+        if (os.toLowerCase().startsWith("win")) {  //如果是Windows系统
+            registry.addResourceHandler("/upload/**")
+                    .addResourceLocations("file:"+dirPath);
+            System.out.println("file:" + dirPath);
+
+        } else {  //linux 和mac
+//            registry.addResourceHandler("/upload/**") //虚拟路劲
+//                    .addResourceLocations("file:" + System.getProperty("user.dir") + "/upload/");//jar 同级目录
+//            System.out.println("file:" + System.getProperty("user.dir") + "/upload/");
+
+            registry.addResourceHandler("/upload/**")
+                    .addResourceLocations("file:"+dirPath);
+            System.out.println("file:" + dirPath);
+        }
+
     }
 
 }
